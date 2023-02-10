ed0ed5758c51a79e500cbd09f05a87614e75e1b9
daniel-kzn <45941342+daniel-kzn@users.noreply.github.com>
2023-02-10 14:21:02 +0100

    :construction: wip: mongo integration

--- server/src/models/TodoSchema.ts
+++ server/src/models/TodoSchema.ts
@@ -1,2 +1,12 @@
 import mongoose, { mongo } from "mongoose";
 const { Schema } = mongoose;
+
+const todoSchema = new Schema({
+  author: String,
+  title: String, // shorthand for {type: Strings}
+  description: String,
+  categorie: String,
+  finish: Boolean,
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
